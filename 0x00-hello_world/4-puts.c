7b3f39785fe35d930c1e38d6f8a9b73e9e68dbd5
Rolex46 <rolexomondi46@gmail.com>
2022-09-09 18:51:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
 *
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
-* Return: 0
+*  Return: 0
 */
 int main(void)
 {
-	puts("\"programming is like building a miltilingual puzzle");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
