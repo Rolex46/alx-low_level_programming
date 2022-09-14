be4a5e6483b44c543ec2eb9c416b415479942725
Rolex46 <rolexomondi46@gmail.com>
2022-09-13 19:01:52 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,9 +2,8 @@
 
 /**
 * main - print putchar
-* Return: always 0
+* Return:always 0
 */
-
 int main(void)
 {
 	_putchar('_');
@@ -15,7 +14,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
-	
+	_putchar('\n');	
 	return (0);
 }
