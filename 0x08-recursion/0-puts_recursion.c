9dc2a58b198a32bac78916a8cdb4d15cd439f617
Rolex46 <rolexomondi46@gmail.com>
2022-09-28 09:32:02 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,13 @@
  *
  * Return: void
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 		_putchar('\n');
 	else
 	{
 		_putchar(s[0]);
-		puts_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
 }
