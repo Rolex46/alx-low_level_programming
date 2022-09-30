3cafe5c01507936c7110071b011ad15f70944811
Rolex46 <rolexomondi46@gmail.com>
2022-09-30 10:15:11 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: counts arguments
+ * @argv: arguments
+ *
+ * Return: always 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
