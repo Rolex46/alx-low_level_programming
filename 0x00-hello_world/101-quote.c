61ccf919629a13073a55ea78704c67e6ec6b6184
Rolex46 <rolexomondi46@gmail.com>
2022-10-11 11:31:29 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+/**
+* print using the putchar funtion
+* description - use the main function
+* print "and that piece of art is useful" - Dora Korpar, 2015-10-19
+* return: 1 
+*/
+
+int main(void)
+{
